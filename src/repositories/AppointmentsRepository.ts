9a3722319bcab873154be17bc5d7cb7ccc322cd0
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2021-08-25 17:06:44 -0400

    Repositorio

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,28 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    return this.appointments.find((appointment) => isEqual(date, appointment.date)) || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
