1aa594ac0c0090505b7f29b6de31686ac01aef8b
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2021-09-19 23:29:11 -0400

    Teste Create Appointment

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { hash } from 'bcryptjs';
 
 import AppError from '@shared/errors/AppError';
 
+import { injectable, inject } from 'tsyringe';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -10,9 +11,12 @@ interface IRequest {
   email: string;
   password: string;
 }
-
+@injectable()
 class CreateUserService {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const checkUserExists = await this.usersRepository.findByEmail(email);
