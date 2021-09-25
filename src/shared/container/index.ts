c27563bc6d04b6664be705fca9f2064d0f9f3c7a
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2021-09-25 14:50:19 -0400

    Criação migration de token

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,5 @@
 import { container } from 'tsyringe';
+
 import '@modules/users/providers';
 import './providers';
 
@@ -7,9 +8,6 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-
-// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -20,8 +18,3 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 );
-
-// container.registerSingleton<IUserTokensRepository>(
-//   'UserTokensRepository',
-//   UserTokensRepository,
-// );
