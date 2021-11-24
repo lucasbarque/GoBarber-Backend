811f349ccf5e70dae8e3b4a5f1e05ed00efa8f08
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2021-11-23 23:39:26 -0400

    Validação de Campos Rotas com celebrate

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,6 +4,8 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import { errors } from 'celebrate';
+
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
@@ -17,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
