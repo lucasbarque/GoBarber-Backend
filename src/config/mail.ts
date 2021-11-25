63c45474c9612a0bf2338136feacfebea6bbcf54
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2021-11-24 20:20:29 -0400

    Email SES

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'contato@lucasbarque.com',
+      name: 'Lucas Barque',
+    },
+  },
+} as IMailConfig;
